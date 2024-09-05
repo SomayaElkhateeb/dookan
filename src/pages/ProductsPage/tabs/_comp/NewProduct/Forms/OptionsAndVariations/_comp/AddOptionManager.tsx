3461f46376bb7e4b636c3fe137bb7f47179e73f8
SomayaElkhateeb <frontend.one@smartivemedia.com>
2024-09-05 09:40:07 +0300

    push in main branch

--- src/pages/ProductsPage/tabs/_comp/NewProduct/Forms/OptionsAndVariations/_comp/AddOptionManager.tsx
+++ src/pages/ProductsPage/tabs/_comp/NewProduct/Forms/OptionsAndVariations/_comp/AddOptionManager.tsx
@@ -1,218 +1,3 @@
-// import { useState } from 'react';
-// import { useTranslation } from 'react-i18next';
-// import { FaCirclePlus } from 'react-icons/fa6';
-// import { Button } from 'src/app/components/optimized';
-// import { useAppSelector } from 'src/app/store';
-// import { useFieldArray, useWatch } from 'react-hook-form';
-// import SpecificAutoCompleteInput from 'src/app/components/ui/SpecificAutoCompleteInput';
-// import SelectFormField from 'src/app/components/ui/form/SelectFormField';
-// import { GlobalDialog } from 'src/app/components/shared';
-// import { ValidFormStoreByValues } from 'src/utils/types';
-// import { RemoveIcon } from 'src/app/utils/icons';
-// import { Values } from '../types';
-// import FormOptionById from './FormOptionById';
-
-
-// interface Props<TFormStore> {
-// 	formStore: ValidFormStoreByValues<TFormStore, Values>;
-// 	label: string;
-// }
-
-// export default function AddOptionManager<TFormStore>({ formStore, label }: Props<TFormStore>) {
-// 	const [open, setOpen] = useState(false);
-// 	const [openFormById, setOpenFormById] = useState(false);
-// 	const [selectedOptions, setSelectedOptions] = useState<any[]>([]);
-// 	const [newOptions, setNewOptions] = useState<any[]>([]);
-// 	const [selectedFieldIndex, setSelectedFieldIndex] = useState<number | null>(null);
-// 	const [selectedOptionId, setSelectedOptionId] = useState<string | null>(null);
-// 	const [selectedOptionName, setSelectedOptionName] = useState<string | null>(null);
-// 	const { t } = useTranslation();
-// 	const { attributes } = useAppSelector((state) => state.attributes);
-
-// 	const { fields, append, remove } = useFieldArray({
-// 		control: formStore.control,
-// 		name: 'variants',
-// 	});
-
-// 	const variants = useWatch({
-// 		control: formStore.control,
-// 		name: 'variants',
-// 	});
-
-// 	const handleAttributeValueComponent = (i: number) => {
-// 		const selectedCode = variants[i]?.code;
-// 		const attributeValue = attributes.find(
-// 			(e) => e?.code?.toString() === selectedCode?.toString()
-// 		);
-
-// 		const options =
-// 			attributeValue?.options?.map((e) => ({
-// 				id: e.id,
-// 				name: e.label,
-// 			})) || [];
-
-// 		return (
-// 			<SpecificAutoCompleteInput
-// 				array={options}
-// 				label={t('Option value')}
-// 				name={`variants[${i}].attributeValues`}
-// 				formStore={formStore}
-// 				onChange={(value) => {
-// 					setNewOptions(value);
-// 				}}
-// 			/>
-// 		);
-// 	};
-
-// 	const dialogStyle = {
-// 		width: { lg: '50%', md: '70%', xs: '90%' },
-// 		height: { md: '350px', xs: '350px' },
-// 	};
-
-// 	const handleOpenDialog = (index: number) => {
-// 		setSelectedFieldIndex(index);
-// 		setOpen(true);
-// 	};
-
-// 	const handleCloseDialog = () => {
-// 		setOpen(false);
-// 		setSelectedFieldIndex(null);
-// 	};
-
-// 	const handleAddOptions = () => {
-// 		setSelectedOptions((prevOptions) => [...prevOptions, ...newOptions]);
-
-// 		if (selectedOptions.length > 0 && newOptions.length > 0) {
-// 			const combinedOptions = [];
-
-// 			for (let selected of selectedOptions) {
-// 				for (let newOption of newOptions) {
-// 					combinedOptions.push({
-// 						combinedName: `${selected.name}-${newOption.name}`,
-// 						selected,
-// 						newOption,
-// 					});
-// 					console.log(`Combined Option: ${selected.name}-${newOption.name}`);
-// 				}
-// 			}
-
-// 			setSelectedOptions(combinedOptions);
-// 		}
-
-// 		setNewOptions([]);
-
-// 		if (selectedFieldIndex !== null) {
-// 			remove(selectedFieldIndex);
-// 		}
-
-// 		handleCloseDialog();
-// 	};
-
-// 	const handleRemoveOption = (event, index: number) => {
-// 		event.stopPropagation();
-// 		setSelectedOptions((prevOptions) =>
-// 			prevOptions.filter((_, i) => i !== index)
-// 		);
-// 	};
-
-// 	const handleOptionClick = (id: string, name: string) => {
-// 		console.log('Option ID:', id);
-// 		console.log('Option Name:', name);
-// 		setSelectedOptionId(id);
-// 		setSelectedOptionName(name);
-// 		setOpenFormById(true);
-// 	};
-
-
-// 	return (
-// 		<div className="flex flex-col gap-5">
-// 			<div className="mt-2">
-// 				<Button
-// 					variant="secondary"
-// 					LeftIcon={FaCirclePlus}
-// 					onClick={() => handleOpenDialog(fields.length)}
-// 				>
-// 					{t('add options')}
-// 				</Button>
-// 			</div>
-
-// 			{fields?.length > 0 &&
-// 				fields.map((item, i) => <div key={i}>{handleAttributeValueComponent(i)}</div>)}
-
-// 			{open && selectedFieldIndex !== null && (
-// 				<GlobalDialog openDialog={open} handleClose={handleCloseDialog} style={dialogStyle}>
-// 					<div className="flex-col-global gap-20">
-// 						<div className="flex-col-global gap-4">
-// 							<h2>{t('Add option to product')}</h2>
-// 							<SelectFormField
-// 								formStore={formStore}
-// 								name={`variants[${selectedFieldIndex}].code`}
-// 								placeholder={t('Select option')}
-// 								label={t('Option Name')}
-// 								AnotherName={`variants[${selectedFieldIndex}].attributeValues`}
-// 								options={
-// 									attributes?.length > 0
-// 										? attributes.map((e) => ({
-// 											value: e.code,
-// 											label: e.name,
-// 										}))
-// 										: []
-// 								}
-// 							/>
-// 							<div className="mt-2">{handleAttributeValueComponent(selectedFieldIndex)}</div>
-// 						</div>
-
-// 						<div className="flex justify-end">
-// 							<Button onClick={handleAddOptions}>{t('add')}</Button>
-// 						</div>
-// 					</div>
-// 				</GlobalDialog>
-// 			)}
-
-// 			<div className="flex flex-col gap-4">
-// 				{selectedOptions.length > 0 &&
-// 					selectedOptions.map((e, index) => {
-// 						const selectedId = e.selected?.id;
-// 						const newOptionId = e.newOption?.id;
-
-// 						const combinedId = selectedId && newOptionId ? `${selectedId}-${newOptionId}` : null;
-
-// 						return (
-// 							<div
-// 								key={index}
-// 								className="cardDetails-sharedClass py-3 px-5 cursor-pointer flex items-center justify-between"
-// 								onClick={() => {
-// 									if (e.combinedName === `${e.selected?.name}-${e.newOption?.name}` && combinedId) {
-// 										handleOptionClick(combinedId, e.combinedName);
-// 									} else {
-// 										console.log('ID not found for the selected option:', e);
-// 									}
-// 								}}
-// 							>
-// 								<p className="fill-title">{e.combinedName || e.name}</p>
-// 								<div onClick={(event) => handleRemoveOption(event, index)}>
-// 									<RemoveIcon className="fill-title cursor-pointer" />
-// 								</div>
-// 							</div>
-// 						);
-// 					})}
-// 			</div>
-
-// 			{/* open form by id */}
-// 			{openFormById && (
-// 				<FormOptionById
-// 					openFormById={openFormById}
-// 					handleClose={() => setOpenFormById(false)}
-// 					selectedOptionId={selectedOptionId}
-// 					selectedOptionName={selectedOptionName} />
-// 			)}
-// 		</div>
-// 	);
-// }
-
-
-
-// ///////////////////////////////////////////////////////
 
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -249,7 +34,7 @@ export default function AddOptionManager<TFormStore>({ formStore, label }: Props
 		name: 'variants',
 	});
 
-	console.log('fields', fields) // []
+	console.log('fields', fields)
 	const variants = useWatch({
 		control: formStore.control,
 		name: 'variants',
@@ -329,9 +114,9 @@ export default function AddOptionManager<TFormStore>({ formStore, label }: Props
 	const handleOptionClick = (id: string, name: string) => {
 		console.log('Option ID:', id);
 		console.log('Option Name:', name);
-		setSelectedOptionId(id); // Set the selected ID
-		setSelectedOptionName(name); // Set the selected name
-		setOpenFormById(true); // Open the form by ID
+		setSelectedOptionId(id);
+		setSelectedOptionName(name);
+		setOpenFormById(true);
 	};
 
 	return (
@@ -413,10 +198,13 @@ export default function AddOptionManager<TFormStore>({ formStore, label }: Props
 				<FormOptionById
 					openFormById={openFormById}
 					handleClose={() => setOpenFormById(false)}
-					selectedOptionId={selectedOptionId} // Pass the selected ID
-					selectedOptionName={selectedOptionName} // Pass the selected Name
+					selectedOptionId={selectedOptionId}
+					selectedOptionName={selectedOptionName}
+					formStore={formStore}
 				/>
 			)}
 		</div>
 	);
 }
+/////////////////////////////////////////////////////////////////////////////
+
--- src/pages/ProductsPage/tabs/_comp/NewProduct/Forms/OptionsAndVariations/_comp/FormOptionById.tsx
+++ src/pages/ProductsPage/tabs/_comp/NewProduct/Forms/OptionsAndVariations/_comp/FormOptionById.tsx
@@ -1,10 +1,19 @@
+import { useFieldArray } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import Textarea from 'src/app/components/optimized/InputsFields/Textarea';
 import { GlobalDialog } from 'src/app/components/shared';
+import FormField from 'src/app/components/ui/form/field';
+import TabbedFormField from 'src/app/components/ui/form/tabbed-field';
+import { Input } from 'src/app/components/ui/input';
+
+
 
 interface FormOptionByIdProps {
     handleClose: () => void;
     openFormById: boolean;
     selectedOptionId: number | null;
     selectedOptionName: string | null;
+    formStore: any;
 }
 
 const FormOptionById = ({
@@ -12,14 +21,19 @@ const FormOptionById = ({
     openFormById,
     selectedOptionId,
     selectedOptionName,
+    formStore,
 }: FormOptionByIdProps) => {
     console.log('Selected Option ID:', selectedOptionId);
     console.log('Selected Option Name:', selectedOptionName);
-
+    const { t } = useTranslation();
     const dialogStyle = {
         width: { lg: '50%', md: '70%', xs: '90%' },
         height: { md: '350px', xs: '350px' },
     };
+    const { fields, remove } = useFieldArray({
+        control: formStore.control,
+        name: 'variations',
+    });
 
     return (
         <GlobalDialog openDialog={openFormById} handleClose={handleClose} style={dialogStyle}>
@@ -27,11 +41,115 @@ const FormOptionById = ({
                 <div className="p-4">
                     <p>Selected Option ID: {selectedOptionId}</p>
                     <p>Selected Option Name: {selectedOptionName}</p>
+                    <div className='flex flex-col gap-1'>
+                        <strong>{t('Stock')}</strong>
+                    </div>
+
+                    {fields.map((item, index) => (
+
+                        <div key={index}>
+
+                            <TabbedFormField
+                                formStore={formStore}
+                                container={{ className: 'flex-grow -translate-x-[13px]' }}
+                                keys={[
+                                    { name: `variations.${index}.en.name`, label: 'En' },
+                                    { name: `variations.${index}.ar.name`, label: 'عربي' },
+                                ]}
+                                renderer={(field) => <Input {...field} />}
+                            />
+
+                            <TabbedFormField
+                                formStore={formStore}
+                                container={{ className: 'flex-grow -translate-x-[13px]' }}
+                                keys={[
+                                    { name: `variations.${index}.en.short_description`, label: 'En' },
+                                    { name: `variations.${index}.ar.short_description`, label: 'عربي' },
+                                ]}
+                                renderer={(field) => <Input {...field} />}
+                            />
+
+                            <TabbedFormField
+                                formStore={formStore}
+                                container={{ className: 'flex-grow -translate-x-[13px]' }}
+                                keys={[
+                                    { name: `variations.${index}.en.description`, label: 'En' },
+                                    { name: `variations.${index}.ar.description`, label: 'عربي' },
+                                ]}
+                                renderer={(field) => <Textarea {...field} />}
+                            />
+                        </div>
+                    ))}
+
+
+
+                    {/* 
+                    <FormField
+                        formStore={props.formStore}
+                        container={{ className: 'flex-grow' }}
+                        name={`variations.${index}.price`}
+                        render={(field) => (
+                            <Input {...field} value={field.value ?? 0} type='number' />
+                        )}
+                    />
+
+                    <FormField
+                        formStore={props.formStore}
+                        container={{ className: 'flex-grow' }}
+                        name={`variations.${index}.sku`}
+                        render={(field) => <Input {...field} />}
+                    />
+
+                    <FormField
+                        formStore={props.formStore}
+                        container={{ className: 'flex-grow' }}
+                        name={`variations.${index}.quantity`}
+                        render={(field) => (
+                            <HorizontalBox end={<>&infin;</>}>
+                                <Input {...field} />
+                            </HorizontalBox>
+                        )}
+                    /> */}
+
+
+
+
+
+
+
+
                 </div>
-            )}
-        </GlobalDialog>
+            )
+            }
+        </GlobalDialog >
     );
 };
 
 export default FormOptionById;
 
+// "variant_0": {
+//     "sku": "Tshirt-hat-red-small1",
+//         "en": {
+//         "name": "Tshirt-hat-Red-Small",
+//             "short_description": "hat red-small desc 1&6",
+//                 "description": "hat color red & size small -1&6"
+//     },
+//     "ar": {
+//         "name": "Tshirt-تي شيرت احمر صغير",
+//             "short_description": "تيشيرت 1&6 وصف صغير",
+//                 "description": "تيشيرت لون احمر مقاس صغير 1&6"
+//     },
+//     "inventories": {
+//         "1": "7"
+//     },
+//     "color": "1",
+//         "size": "6",
+//             "new": "1",
+//                 "featured": "1",
+//                     "visible_individually": "1",
+//                         "price": "100",
+//                             "weight": "10",
+//                                 "status": "1",
+//                                     "discount": "100"
+// },
+// 
